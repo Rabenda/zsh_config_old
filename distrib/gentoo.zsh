570d6f6f0af994e550cc34485ed080141d0d11da
Revy <rabenda.cn@gmail.com>
2021-10-31 23:19:07 +0800

    Update config
    Signed-off-by: Revy <rabenda.cn@gmail.com>

--- distrib/gentoo.zsh
+++ distrib/gentoo.zsh
@@ -4,10 +4,25 @@ function gen-update-index() {
   sudo eix-sync -a
 }
 
+alias gui=gen-update-index
+
 function gen-update() {
   sudo emerge -avuD --keep-going @world
 }
 
+alias gu=gen-update
+
 function gen-rebuild() {
   sudo emerge -avej --update --keep-going @world
 }
+
+alias gr=gen-rebuild
+
+function gen-kernel-update() {
+  kernel_index=$(eselect kernel list | grep linux | tail -n 1 | awk '{print $1;}' | tr '[]' '  ' | awk '{print $1;}')
+  sudo eselect kernel set $kernel_index
+  sudo cp /root/kernelconfig /usr/src/linux/.config
+  sudo genkernel all
+}
+
+alias gku=gen-kernel-update
